b16d52fef724ae03944a3403df0218b85e8fbab4
= <=>
2024-09-12 18:14:22 +0200

    refactor

--- main.go
+++ main.go
@@ -41,26 +41,22 @@ func trimEmpty(key string, jsonParsed *gabs.Container, parent *gabs.Container) {
 	}
 }
 
-var myscheme *runtime.Scheme
-var decoder runtime.Decoder
-
-func main() {
+func defaultsForResource(resource []byte) *gabs.Container {
+	var myscheme *runtime.Scheme
+	var decoder runtime.Decoder
 
 	myscheme = runtime.NewScheme()
 	apisv1.AddToScheme(myscheme)
 	decoder = scheme.Codecs.UniversalDeserializer()
 
-	input, err := io.ReadAll(os.Stdin)
-	defInput := make([]byte, len(input))
-	copy(defInput, input)
+	defaultB := make([]byte, len(resource))
+	copy(defaultB, resource)
 
+	defaultJson, err := gabs.ParseJSON(defaultB)
 	if err != nil {
 		panic(err)
 	}
 
-	candidate, _, _ := decoder.Decode(defInput, nil, nil)
-	deflt, _ := json.Marshal(candidate)
-	defaultJson, _ := gabs.ParseJSON(deflt)
 	defaultFlat, _ := defaultJson.Flatten()
 	for key, _ := range defaultFlat {
 		if strings.HasPrefix(key, "spec.") {
@@ -70,25 +66,39 @@ func main() {
 	}
 
 	rawDefault, _ := json.Marshal(defaultJson.Data())
-	candidate2, _, _ := decoder.Decode(rawDefault, nil, nil)
-	myscheme.Default(candidate2)
-	deflt2, _ := json.Marshal(candidate2)
-	defaultJson2, _ := gabs.ParseJSON(deflt2)
+	candidate, _, _ := decoder.Decode(rawDefault, nil, nil)
+	myscheme.Default(candidate)
+	default_, _ := json.Marshal(candidate)
+	defaultJson2, _ := gabs.ParseJSON(default_)
+
+	return defaultJson2
+}
+
+func main() {
+
+	input, err := io.ReadAll(os.Stdin)
+
+	if err != nil {
+		panic(err)
+	}
 
 	inputJson, err := gabs.ParseJSON(input)
 	if err != nil {
 		panic(err)
 	}
+	defaultJson := defaultsForResource(input)
 
 	flat, _ := inputJson.Flatten()
 	for key, _ := range flat {
 		left := inputJson.Path(key).Data()
-		right := defaultJson2.Path(key).Data()
+		right := defaultJson.Path(key).Data()
 
 		if left == right {
 			inputJson.DeleteP(key)
 		}
 	}
+
+	inputJson.DeleteP("status")
 
 	trimEmpty("", inputJson, nil)
 
